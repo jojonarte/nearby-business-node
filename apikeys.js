3c88271af61f495fca7a0249c01b9dbd5cb55f0d
Jojo Narte <jojonarte@users.noreply.github.com>
2016-07-19 14:54:51 +0800

    remove keys

--- apikeys.js
+++ apikeys.js
@@ -1,16 +1,16 @@
 exports.foursquarekeys = {
-    clientId: 'DJ0USWOSSOMKFIXTUVYY3RDUNVP2QSIQJEC2V0D5WG1QMY4X',
-    clientSecret: '1QDIDJ4JM0MIVVHWSP1MVVT42PX0CUPTPYXNWKY2LGK2JVIB',
+    clientId: '',
+    clientSecret: '',
 };
 
 exports.googlekeys = {
-    apiKey : "AIzaSyB8H6Shvv5l-5LNLOeEPl1F2icPwu6sUuI",
+    apiKey : '',
     outputFormat : "json",
 };
 
 exports.yelpkeys = {
-    consumer_key: "LV7tkzHOBNCUHJARsYL-_A",
-    consumer_secret: "barKjlEC8nIjn1YkFEpCEYUr4Fo",
-    token: "0bix1T0kY9cQK2POiQeSDFgDkHp8ZYi9",
-    token_secret: "SQ3LZOpmCPTM_bOlOdi1xrwJfxY"
+    consumer_key: '',
+    consumer_secret: '',
+    token: '',
+    token_secret: ''
 };
